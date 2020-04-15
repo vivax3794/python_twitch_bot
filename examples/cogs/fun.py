78db3dee25c05211cabca0b47059eec23ae8ab27
vivax <ole_egil@outlook.com>
2020-04-15 20:14:57 +0200

    orginized the example folder

--- examples/cogs/fun.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def setup(bot):
-    @bot.command()
-    def dice(ctx):
-        num = random.randint(0, 10)
-        ctx.reply(num)
--- examples/cogs/info.py
+++
@@ -1,4 +0,0 @@
-def setup(bot):
-    @bot.command()
-    def project(ctx):
-        ctx.reply("I am working on a twitch bot in python, github link: https://github.com/vivax3794/python_twitch_bot ") 
