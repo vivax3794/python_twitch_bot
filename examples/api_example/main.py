e5578f3e265cb4ed9de99018c778a2e7fb928aeb
vivax3794 <vivax3794@users.noreply.github.com>
2020-04-17 17:29:10 +0200

    added a example of using the api

---
+++ examples/api_example/main.py
@@ -0,0 +1,22 @@
+from PyTwitch import TwitchBot
+
+from secret import TOKEN, CLIENT_ID
+# client id can be gotten by making a application at: https://dev.twitch.tv/console/apps
+
+BOT_NAME = "therealvivax"
+CHANNEL = "vivax3794"
+
+bot = TwitchBot()
+bot.connect(BOT_NAME, TOKEN)
+
+channel = bot.join_channel(CHANNEL)
+
+@bot.command()
+def followers(ctx):
+    followers_list = ctx.channel.followers
+    ctx.reply(f"{channel.name} has {len(followers_list)} followers")
+    # NOTE: there is a more efficent way to get this with the api that will be added latter.
+    # TODO: remove note once the above mentioned is added
+
+if __name__ == "__main__":
+    bot.run()
