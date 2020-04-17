a551f6e8a17ce8a23c6a1fb16a0be58aa31388a1
vivax3794 <vivax3794@users.noreply.github.com>
2020-04-17 17:29:49 +0200

    added a commet in the api exmaple

--- examples/api_example/main.py
+++ examples/api_example/main.py
@@ -13,6 +13,7 @@ channel = bot.join_channel(CHANNEL)
 
 @bot.command()
 def followers(ctx):
+    # this attribute uses the api
     followers_list = ctx.channel.followers
     ctx.reply(f"{channel.name} has {len(followers_list)} followers")
     # NOTE: there is a more efficent way to get this with the api that will be added latter.
