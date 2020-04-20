5fba3dadeba7d28a0df34ae8e98f1b751c80108f
vivax3794 <vivax3794@users.noreply.github.com>
2020-04-20 16:58:11 +0200

    updated example to use Channel.num_followers

--- examples/api_example/main.py
+++ examples/api_example/main.py
@@ -14,10 +14,8 @@ channel = bot.join_channel(CHANNEL)
 @bot.command()
 def followers(ctx):
     # this attribute uses the api
-    followers_list = ctx.channel.followers
-    ctx.reply(f"{channel.name} has {len(followers_list)} followers")
-    # NOTE: there is a more efficent way to get this with the api that will be added latter.
-    # TODO: remove note once the above mentioned is added
+    followers_ammount = ctx.channel.num_followers
+    ctx.reply(f"{channel.name} has {followers_ammount} followers")
 
 if __name__ == "__main__":
     bot.run()
