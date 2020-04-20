346768f5a789bcfa6def62888651ecd22bbf1ca1
vivax3794 <vivax3794@users.noreply.github.com>
2020-04-20 16:05:34 +0200

    moved all errors to one file

---
+++ PyTwitch/errors.py
@@ -0,0 +1,30 @@
+class ApiError(Exception):
+    """
+    A error happend with the api.
+    """
+
+
+class ResponseCodeError(ApiError):
+    """
+    The response code of a request was not excpected.
+    """
+
+
+class NoClientId(ApiError):
+    """
+    Client id is needed for this request
+    """
+    def __init__(self) -> None:
+        super().__init__("Client id is needed for this request")
+
+
+class RatelimitError(ApiError):
+    """
+    Twitch rate limit reached.
+    """
+
+
+class StreamerNotLiveError(ApiError, ValueError):
+    """
+    The streamer is offline.
+    """
--- PyTwitch/twitch_api.py
+++ PyTwitch/twitch_api.py
@@ -6,6 +6,8 @@ import warnings
 
 import requests
 
+from .errors import NoClientId, RatelimitError, ResponseCodeError, StreamerNotLiveError
+
 # types
 UserInfo = TypedDict("UserInfo", {
     "id": str,
@@ -48,36 +50,6 @@ ApiRespons = TypedDict("ApiRespons", {
     })
 
 
-class ApiError(Exception):
-    """
-    A error happend with the api.
-    """
-
-
-class ResponseCodeError(ApiError):
-    """
-    The response code of a request was not excpected.
-    """
-
-
-class NoClientId(ApiError):
-    """
-    Client id is needed for this request
-    """
-    def __init__(self) -> None:
-        super().__init__("Client id is needed for this request")
-
-
-class RatelimitError(ApiError):
-    """
-    Twitch rate limit reached.
-    """
-
-
-class StreamerNotLiveError(ApiError, ValueError):
-    """
-    The streamer is offline.
-    """
 
 
 class TwitchApi:
